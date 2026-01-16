0ed33efb2b0cdfa2dfc184f50ebb4cf13e2f4c7e
patsaporn-phadungsil <68010852@kmitl.ac.th>
2026-01-16 10:17:43 +0700

    Update index.ts

--- src/practical/p01/index.ts
+++ src/practical/p01/index.ts
@@ -1,1 +1,28 @@
-export function getEdgePosts() {}
+import axios from "axios";
+
+type Post = {
+  id: number;
+  title: string;
+};
+
+export async function getEdgePosts(): Promise<Post[]> {
+  try {
+    const res = await axios.get<Post[]>(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+
+    const posts = res.data;
+
+    if (posts.length === 0) return [];
+
+    if (posts.length === 1) {
+      const {id,title} posts[0];
+      return [{id,title},
+        {id,tile}];
+  }
+  const frist = posts[0];
+}
+  const last = posts[posts.length - 1];
+  return [{id,title},
+    {id,tile}];
+  }
