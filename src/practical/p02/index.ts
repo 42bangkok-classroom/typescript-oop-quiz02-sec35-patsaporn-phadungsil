4e7ba9e51c0fae4eb6ebcd181103642f21dbcffb
patsaporn-phadungsil <68010852@kmitl.ac.th>
2026-01-16 10:36:13 +0700

    Update index.ts

--- src/practical/p02/index.ts
+++ src/practical/p02/index.ts
@@ -1,1 +1,26 @@
-export function getPostsByUser() {}
+import axios from "axios";
+
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+};
+
+type ResultPost = {
+  id: number;
+  title: string;
+};
+
+export async function getPostsByUser(userId: number): Promise<ResultPost[]> {
+  try {
+    const res = await axios.get<Post[]>(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+
+    return res.data
+      .filter((post) => post.userId === userId)
+      .map(({ id, title }) => ({ id, title }));
+  } catch (error) {
+    throw error;
+  }
+}
