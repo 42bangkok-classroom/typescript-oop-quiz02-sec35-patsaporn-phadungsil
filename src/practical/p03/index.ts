69e3f9deb88f382219d2061bfea86618a07b42d2
patsaporn-phadungsil <68010852@kmitl.ac.th>
2026-01-16 10:47:16 +0700

    commit

--- src/practical/p03/index.ts
+++ src/practical/p03/index.ts
@@ -1,1 +1,45 @@
-export async function mapPostWithCommentCount(Post: number) {}
+import axios from "axios";
+
+type Post = {
+  id: number;
+  title: string;
+};
+
+type Comment = {
+  postId: number;
+  id: number;
+};
+
+type ResultPost = {
+  postId: number;
+  title: string;
+  totalComments: number;
+};
+
+export async function mapPostWithCommentCount(): Promise<ResultPost[]> {
+  try {
+    const [postRes, commentRes] = await Promise.all([
+      axios.get<Post[]>("https://jsonplaceholder.typicode.com/posts"),
+      axios.get<Comment[]>("https://jsonplaceholder.typicode.com/comments"),
+    ]);
+
+    const posts = postRes.data;
+    const comments = commentRes.data;
+
+    if (posts.length === 0) return [];
+
+    return posts.map((post) => {
+      const totalComments = comments.filter(
+        (c) => c.postId === post.id
+      ).length;
+
+      return {
+        postId: post.id,   // ✅ rename id → postId
+        title: post.title,
+        totalComments,
+      };
+    });
+  } catch (error) {
+    throw error;
+  }
+}
